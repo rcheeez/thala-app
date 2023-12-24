238b91fd784d721f2f0904aa5a38486bc890ecd5
Archies Gurav <archiesgurav10@gmail.com>
2023-12-24 12:47:28 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import ReactPlayer from 'react-player';
 import './App.css';
 
 function App() {
+  const [inputText, setInputText] = useState('');
+  const [output, setOutput] = useState('');
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [len, setLen] = useState(0);
+  const [formattedText, setFormattedText] = useState('');
+
+  const handleChange = (e) => {
+    const newText = e.target.value;
+    setInputText(newText);
+
+    if (newText.length === 7) {
+      setLen(newText.length);
+      setOutput('THALA FOR A REASON');
+      setIsPlaying(true);
+    } else {
+      setOutput('');
+      setIsPlaying(false);
+    }
+
+      setFormattedText(newText.toUpperCase().split('').join('+'));
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Thala Project</h1>
+      <input
+        type="text"
+        value={inputText}
+        onChange={handleChange}
+        placeholder="Enter a 7 Letter Word"
+        maxLength={7}
+      />
+      <br/>
+      <h3>{output && `${formattedText} = ${len}`}</h3>
+      {output && <h1 className='output'>{output}</h1>}
+
+      <ReactPlayer
+        url="http://localhost:3000/thala.mp4" 
+        playing={isPlaying}
+        controls={false}
+        width="100%"
+      />
     </div>
   );
 }
